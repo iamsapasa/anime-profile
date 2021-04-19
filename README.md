9497f6a312575670db0a6030c5f48001ffdad0d4
Sa Pasa <sa.pasa@fdmgroup.com>
2021-04-19 23:14:22 +1000

    displays image of users anime

--- README.md
+++ README.md
@@ -10,10 +10,16 @@ sample GET, https://api.jikan.moe/v3/user/Akiimai/animelist/all
 # step 1 create project
 npx create-react-app malpro
 cd malpro
-npm start
 
 # step 2, add a ui lib for the layout
 npm install @material-ui/core
 
+# step 3, start project
+npm start
+
+# step 4, react stuff
+
+# hsl selector
+https://hslpicker.com/#08010f,0.81
 
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+const axios = require('axios');
+
+
+const AnimeCards = () => {
+  const [animes, setAnimes] = useState([{ title: 'Haikyuu' }, { title: 'AOT' }])
+  const [isLoading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const username = 'Akiimai';
+    axios.get(`https://api.jikan.moe/v3/user/${username}/animelist/all`)
+      .then(function (response) {
+        setAnimes(response.data.anime)
+        setLoading(false);
+      });
+
+  }, [])
+
+  switch (isLoading) {
+    case true:
+      return <p>Loading</p>;
+
+    default:
+      return Object.entries(animes).map(([key, values]) => 
+      <AnimeCard 
+      title={values.title} 
+      imageUrl={values.image_url}
+      score={values.score}
+       />);
+  }
+}
+
+
+
+const AnimeCard = ({ title, imageUrl, score }) => {
+  return (
+    <Grid item xs={4} sm={3} md={2} >
+      <div className='card'>
+        <div className='image'> <img src={imageUrl} /></div>
+        <div className='rating'><p>{score}/10</p></div>
+        <div className='title'> <p>{title}</p></div>
+      </div>
+    </Grid>
+  );
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container >
+      <AnimeCards />
+    </Grid>
   );
 }
 
