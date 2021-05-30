b546c2b2d04a52180521ccfc885fe7f1b6da1a2e
Sa Pasa <sa.pasa@fdmgroup.com>
2021-05-30 18:42:27 +1000

    updated README and cleaned up

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # anime-profile
-this is a simple ui tool to view a persons anime list
+this is a simple ui tool to view a persons my-anime-list
 
 # using this api
 https://jikan.docs.apiary.io/#reference/0/user
@@ -7,19 +7,71 @@ https://jikan.docs.apiary.io/#reference/0/user
 sample GET, https://api.jikan.moe/v3/user/Akiimai/animelist/all
 
 
-# step 1 create project
+# Coding from scratch:
+
+### Create project
+```
 npx create-react-app malpro
 cd malpro
+```
 
-# step 2, add a ui lib for the layout
+### Install Libraries
+```
 npm install @material-ui/core
+npm install react-router-dom
+npm install axios
+```
 
-# step 3, start project
+### Run project
+```
 npm start
+```
 
-# step 4, react stuff
+###  Code
+This is React boiler plate code for `App.js`
+```
+import React from 'react';
 
-# hsl selector
-https://hslpicker.com/#08010f,0.81
+const Component = () => {
+  useEffect(() => {
+   
+  }, [])
 
+  return <p>Loading...</p>;
+}
+
+function App() {
+  return (
+   <Component>
+  );
+}
+
+export default App;
+
+```
+This is how you make a REST call using `axios`
+```
+axios.get(`https://api.jikan.moe/v3/user/${username}/animelist/all`)
+    .then(function (response) {
+    console.log('response recieved', response)
+    });
+```
+This is how you use `.map()` over an array
+```
+const names = ["whale", "squid", "turtle", "coral", "starfish"];
+
+const NamesList = () => (
+  <div>
+    <ul>{names.map(name => 
+        <li key={name}> {name} </li>)}
+    </ul>
+  </div>
+);
+
+```
+
+# Useful links
+[Transparent HSL color selector ](https://hslpicker.com/#08010f,0.81)
+
+[How to position stuff using position:absolute and position:relative](https://css-tricks.com/absolute-positioning-inside-relative-positioning/)
 
--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
-
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, useParams } from "react-router-dom";
 import './App.css';
 import Grid from '@material-ui/core/Grid';
 const axios = require('axios');
 
-
 const AnimeCards = () => {
-  const [animes, setAnimes] = useState([{ title: 'Haikyuu' }, { title: 'AOT' }])
+  const [animes, setAnimes] = useState([])
   const [isLoading, setLoading] = useState(true);
-
+  let { username } = useParams();
 
   useEffect(() => {
-    const username = 'Akiimai';
+    username = username ? username : 'Akiimai';
     axios.get(`https://api.jikan.moe/v3/user/${username}/animelist/all`)
       .then(function (response) {
+        console.log('response recieved', response)
         setAnimes(response.data.anime)
         setLoading(false);
       });
-
   }, [])
 
   switch (isLoading) {
     case true:
-      return <p>Loading</p>;
-
+      return <p>Loading...</p>;
     default:
-      return Object.entries(animes).map(([key, values]) => 
-      <AnimeCard 
-      title={values.title} 
-      imageUrl={values.image_url}
-      score={values.score}
-       />);
+      return (
+        <Grid container>
+          {
+            animes.map((anime) =>
+              <AnimeCard
+                key={anime.mal_id}
+                title={anime.title}
+                imageUrl={anime.image_url}
+              />)
+          }
+        </Grid>)
   }
 }
 
-
-
-const AnimeCard = ({ title, imageUrl, score }) => {
+const AnimeCard = ({ title, imageUrl }) => {
   return (
     <Grid item xs={4} sm={3} md={2} >
       <div className='card'>
-        <div className='image'> <img src={imageUrl} /></div>
-        <div className='rating'><p>{score}/10</p></div>
-        <div className='title'> <p>{title}</p></div>
+        <p className='title'>{title}</p>
+        <img className='image' src={imageUrl} alt={imageUrl} />
       </div>
     </Grid>
   );
 }
 
 function App() {
-
   return (
-    <Grid container >
-      <AnimeCards />
-    </Grid>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <AnimeCards />
+        </Route>
+        <Route path="/:username">
+          <AnimeCards />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
